ec6587477d98ab486fe44c877e50e3186cc19024
Gabriel Hejduk <148792329+Gabcioo@users.noreply.github.com>
2023-11-01 20:44:07 +0100

    Add files via upload

---
+++ 01_intro.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 28 10:23:01 2023
+
+@author: macbookgabriel
+"""
+
+
+# %%
+print(2 + 2)
+
+# %%    
+print(3 * 3)
+
+# %%
+print(3 + 2 * 2)
+print(4 - 2 * 2)
+# trzeba podać print bo inaczej nie pokażą się wszystkie działanie
+
+# %%
+10 / 3
+4 / 2
+
+# %%
+10 // 3
+7 // 6
+8 // 4
+
+# %%
+2 ** 5
+
+# %%
+10 % 3
+12 % 5
+# zwraca reszte dzielenia
+
+# %%
+(10 - 2 ** 3) * 10
+
+# %%
+a = 10
+b = 20
+
+c = a * b
+print(c)
+
+# %%
+print('love python')
+
+
+# %%
+print('it\'s the best!')
+
+# %%
+print('python 3.11')
+print('python\n3.11')
+
+# %%
+print("""python
+3.7""")
+
+# %%
+a = 10  
+
+# %%
+print('\t\t\tPython')
+
+# %%
+print('C:\path\\to\something\\new') 
+# %%
+print(r'C:\path\to\something\new') 
+# %%
+import os
+os.getcwd()
+
+# %%
+print("""
+Instrukcja uruchamiania pliku przyklad.py:
+
+    --file [nazwa pliku]
+      zapisuje output do pliku 
+      
+    --quiet
+        wycisza logi w konsoli
+        
+Koniec.""")
+
+# %%
+text = 'I love python. '
+print(text)
+print('hau...' * 10)
+print('_' * 30)
+
+# %%
+'python'
+'Py' 'thon'
+print('py' 'thon')
+
+# %%
+url = ('jakibardzodlugiznakktorywychodzipozaznakiafiusdhajfvkshdkjvnkjcndkjsvv'
+       'nfjkdnvfdkj')
+
+# %%
+name = 'python'
+print(name, '3.7')
+
+# %%
+age = 27
+imie = 'paweł'
+
+print(imie + ' ' + str(age))
+print(imie, age)
+print('{0} ma {1} lat.'.format(imie, age))
+# %%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
